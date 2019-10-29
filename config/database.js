1dc5ece4aa826e9279bac74351d27b68185cf275
KoinuKoji <codymcox@gmail.com>
2019-10-29 07:09:52 -0500

    Initial Commit

---
+++ config/database.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+
+const CONNECTION_URL = "mongodb+srv://meansquares:Blockchain2019@nraboy-sample-a22jr.mongodb.net/test?retryWrites=true";
+const DATABASE_NAME = "blockchain";
+
+var app = express();
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+var database, collection;
+
+app.listen(3000, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("users");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
